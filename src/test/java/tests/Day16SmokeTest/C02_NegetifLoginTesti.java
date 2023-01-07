ed4ff2276fa2c0b90b61959e75397a1bcf96ec82
Faruk Yucel <farukyucel5@gmail.com>
2023-01-07 12:27:19 +0300

    some amendmends was made

---
+++ src/test/java/tests/Day16SmokeTest/C02_NegetifLoginTesti.java
@@ -0,0 +1,66 @@
+package tests.Day16SmokeTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+import utilities.Reuseable;
+import utilities.configReader;
+
+public class C02_NegetifLoginTesti {
+    // 3 test method'u olusturun
+    // 1.de yanlis email, dogru sifre
+    // 2.de dogru email yanlis sifre
+    // 3.de yanlis email, yanlis sifre ile giris yapmayi deneyin
+    // giris yapilamadigini test edin
+
+    QualitydemyPage qualitydemyPage= new QualitydemyPage();
+
+    @Test
+    public void yanlisEmailTesti(){
+
+        Driver.getDriver().get(configReader.getProperty("qdUrl"));
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys(configReader.getProperty("qdGecersizUsername"));
+        qualitydemyPage.passwordKutusu.sendKeys(configReader.getProperty("qdGecerliPassword"));
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+
+        Reuseable.bekle(3);
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void yanlisPasswordTesti(){
+        qualitydemyPage= new QualitydemyPage();
+        Driver.getDriver().get(configReader.getProperty("qdUrl"));
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys(configReader.getProperty("qdGecerliUsername"));
+        qualitydemyPage.passwordKutusu.sendKeys(configReader.getProperty("qdGecersizPassword"));
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+
+        Reuseable.bekle(3);
+        Driver.closeDriver();
+
+    }
+
+
+    @Test
+    public void yanlisUsernameYanlisPasswordTesti(){
+
+        qualitydemyPage= new QualitydemyPage();
+        Driver.getDriver().get(configReader.getProperty("qdUrl"));
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys(configReader.getProperty("qdGecersizUsername"));
+        qualitydemyPage.passwordKutusu.sendKeys(configReader.getProperty("qdGecersizPassword"));
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Reuseable.bekle(3);
+        Driver.closeDriver();
+    }
+
+}
